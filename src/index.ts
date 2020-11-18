80d2e46870d4ab79f1dba70fd1bdbcb0d6041050
lebowvsky <lebowvsky@gmail.com>
2020-11-18 14:29:12 +0100

    Init ApolloServer with a fake bookResolver

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import 'reflect-metadata';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+import { createConnection } from 'typeorm';
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+const bookResolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const main = async () => {
+  await createConnection();
+
+  const server = new ApolloServer({ typeDefs, resolvers: bookResolvers });
+
+  const app = express();
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+  console.log('Server has started!');
+};
+
+main();
