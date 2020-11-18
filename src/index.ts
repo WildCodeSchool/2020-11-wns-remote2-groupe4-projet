e81980775b57209d4c6b7b8e29601f17c605b642
Brice Le Gallo <lebowvsky@gmail.com>
2020-11-18 16:48:59 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,6 @@ const main = async () => {
   app.listen({ port: 4000 }, () =>
     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
   );
-  console.log('Server has started!');
 };
 
 main();
