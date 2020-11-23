a0ecdc933e0041f9535fc6323d4726cdf454d08c
lebowvsky <lebowvsky@gmail.com>
2020-11-23 18:37:10 +0100

    Create & Stylize AvatarCpnt

--- web-client/src/containers/left-aside-ctnr/LeftAsideCtnr.tsx
+++ web-client/src/containers/left-aside-ctnr/LeftAsideCtnr.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import AvatarCpnt from '../../components/avatar-cpnt/AvatarCpnt';
 const LeftAsideCtnr = (): JSX.Element => {
-  return <aside className="left-aside"></aside>;
+  return (
+    <aside className="left-aside">
+      <AvatarCpnt />
+    </aside>
+  );
 };
 
 export default LeftAsideCtnr;
