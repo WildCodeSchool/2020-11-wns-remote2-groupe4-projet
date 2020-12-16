54b67a5628623a8874466506d5a0321b1e272d89
Florian <florian-murihano_student2020@wilder.school>
2020-12-16 10:51:32 +0100

    creating right side panel

--- web-client/src/containers/MainCtnr.tsx
+++ web-client/src/containers/MainCtnr.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { RouteProps } from 'react-router-dom';
+
 import LeftAsideCtnr from './left-aside-ctnr/LeftAsideCtnr';
+import RightAsideCtnr from './right-aside-ctnr/RightAsideCtnr';
 
 const MainCtnr = ({ children }: RouteProps): JSX.Element => {
   return (
     <main className="main-ctrn">
       <LeftAsideCtnr />
       {children}
-      <div>Bandeau droit</div>
+      <RightAsideCtnr />
     </main>
   );
 };
---
+++ web-client/src/containers/right-aside-ctnr/RightAsideCtnr.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './RightAsideCtnr.scss';
+
+const RightAsideCtnr = (): JSX.Element => {
+  return (
+    <aside className="right-aside">
+      <div>Chat</div>
+    </aside>
+  );
+};
+
+export default RightAsideCtnr;
