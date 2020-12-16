496b75e2f2008afd496da5dc22ea6ba479d59d3e
lebowvsky <lebowvsky@gmail.com>
2020-12-16 14:04:12 +0100

    style(LeftAsideCtnr) Add some style and make it responsive

--- web-client/src/components/avatar-cpnt/AvatarCpnt.tsx
+++ web-client/src/components/avatar-cpnt/AvatarCpnt.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-const AvatarCpnt = (): JSX.Element => {
+type AvatarProps = {
+  isLeftAsideOpen: boolean;
+};
+
+const AvatarCpnt = ({ isLeftAsideOpen }: AvatarProps): JSX.Element => {
   return (
     <div className="avatar">
       <img
-        className="a-image"
+        className={`a-image ${!isLeftAsideOpen && 'a-image-small'}`}
         src="https://directemployers.org/wp-content/uploads/2018/08/avatar-JohnDoe.jpg"
         alt="avatar"
       ></img>
-      <h3 className="a-name">John Doe</h3>
+      <h3 className={`a-name ${!isLeftAsideOpen && 'hide-name'}`}>John Doe</h3>
     </div>
   );
 };
