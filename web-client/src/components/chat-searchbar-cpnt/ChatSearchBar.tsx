a57b5967b694d8e1781f8ff4c29fbc5af67a7e0b
Florian <florian-murihano_student2020@wilder.school>
2020-12-18 10:26:00 +0100

    add auto-complete to the chat  search bar

--- web-client/src/components/chat-searchbar-cpnt/ChatSearchBar.tsx
+++ web-client/src/components/chat-searchbar-cpnt/ChatSearchBar.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import './ChatSearchBar.scss';
 
 const ChatSearchBar = (): JSX.Element => {
   return (
     <div className="chat-searchbar-wrapper">
-      <TextField
-        id="outlined-search"
-        label="Recherche"
-        size="small"
-        type="search"
-        variant="outlined"
+      <Autocomplete
+        id="autocomplete-searchbar"
+        options={[
+          { channelTitle: 'Option1' },
+          { channelTitle: 'Option2' },
+          { channelTitle: 'Option3' },
+          { channelTitle: 'Option4' },
+          { channelTitle: 'Test' },
+        ]}
+        getOptionLabel={(option) => option.channelTitle}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            id="outlined-textfield-searchbar"
+            label="Recherche"
+            size="small"
+            type="search"
+            variant="outlined"
+          />
+        )}
       />
     </div>
   );
