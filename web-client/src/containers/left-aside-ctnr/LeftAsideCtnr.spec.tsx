80aa66314ce6fbc6a777f9e9f964cbab22afa873
lebowvsky <lebowvsky@gmail.com>
2021-04-08 16:42:34 +0200

    fix(LefAsideCtnr.spec) add MockProvider in test Co-authored-by: AhmadSAFAR <AhmadSAFAR@users.noreply.github.com> Co-authored-by: Code-Hello <Code-Hello@users.noreply.github.com>

--- web-client/src/containers/left-aside-ctnr/LeftAsideCtnr.spec.tsx
+++ web-client/src/containers/left-aside-ctnr/LeftAsideCtnr.spec.tsx
@@ -1,44 +1,55 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
 import LeftAsideCtnr from './LeftAsideCtnr';
 
 describe('LeftAsideCntr by default', () => {
+  beforeEach(() => {
+    render(
+      <MockedProvider>
+        <LeftAsideCtnr />
+      </MockedProvider>
+    );
+  });
+
   it('Display the avatar', () => {
-    render(<LeftAsideCtnr />);
     const avatar = document.querySelector('.avatar');
     expect(avatar).toBeInTheDocument();
   });
 
   it('Display the menu', () => {
-    render(<LeftAsideCtnr />);
     const menu = document.querySelector('.menu');
     expect(menu).toBeInTheDocument();
   });
 
   it('Display the menu buttons', () => {
-    render(<LeftAsideCtnr />);
     const buttonsMenu = document.querySelector('.btn-wrapper');
     expect(buttonsMenu).toBeInTheDocument();
   });
 });
 
 describe('when button clicked', () => {
+  beforeEach(() => {
+    render(
+      <MockedProvider>
+        <LeftAsideCtnr />
+      </MockedProvider>
+    );
+  });
+
   it('Hides the avatar name', () => {
-    render(<LeftAsideCtnr />);
     const buttonCpnt = screen.getByTestId('buttonCpntTested');
     fireEvent.click(buttonCpnt);
     expect(screen.getByTestId('avatar-name').classList).toContain('hide-name');
   });
 
   it('Makes image smaller', () => {
-    render(<LeftAsideCtnr />);
     const buttonCpnt = screen.getByTestId('buttonCpntTested');
     fireEvent.click(buttonCpnt);
     expect(screen.getByRole('img').classList).toContain('a-image-small');
   });
 
   it('Rotates LeftAsideCtnr icon', () => {
-    render(<LeftAsideCtnr />);
     const buttonCpnt = screen.getByTestId('buttonCpntTested');
     fireEvent.click(buttonCpnt);
     expect(buttonCpnt.classList).toContain('rotate-left');
