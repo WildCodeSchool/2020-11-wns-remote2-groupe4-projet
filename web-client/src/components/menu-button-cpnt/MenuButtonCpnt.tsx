7745594ddc4dcc19e0055538a64e8ff9d5a93c4e
Brice <lebowvsky@gmail.com>
2021-07-08 12:25:56 +0200

    style(MenuButtonCpnt) :lipstick: change button style
    Co-authored-by: Code-Hello <Code-Hello@users.noreply.github.com>

--- web-client/src/components/menu-button-cpnt/MenuButtonCpnt.tsx
+++ web-client/src/components/menu-button-cpnt/MenuButtonCpnt.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { useMutation } from '@apollo/client';
 import { useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faPowerOff } from '@fortawesome/free-solid-svg-icons';
 
 import { DELETE_SESSION } from '../../queries/sessionQueries';
 
@@ -28,11 +30,11 @@ const MenuButtonCpnt = ({ isLeftAsideOpen, onItemClick }: MenuButtonProps): JSX.
     <div className={`btn-wrapper ${!isLeftAsideOpen && 'btn-wrapper-column'}`}>
       <Link to="parameters">
         <button className="menu-btn parameters" onClick={onItemClick}>
-          Paramètres
+          <FontAwesomeIcon className="mb-icon" icon={faCog} />
         </button>
       </Link>
       <button className="menu-btn deconnexion" onClick={logoutClick}>
-        Déconnexion
+        <FontAwesomeIcon className="mb-icon" icon={faPowerOff} />
       </button>
     </div>
   );
